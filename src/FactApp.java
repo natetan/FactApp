c13d405c30ca0bb96bd2c3148de80ade8051448f
yulongtan <swifties@uw.edu>
2015-05-26 23:40:33 -0700

    created a facts program

---
+++ src/FactApp.java
@@ -0,0 +1,80 @@
+// Author: Yulong Tan
+// Date: 5.23.15
+// This program gets random facts ad returns them.
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class FactApp {
+	private String[] facts;
+
+	// Initializes the fields
+	public FactApp() {
+		this.facts = null;
+	}
+
+	// This method takes in a Scanner as a parameter and
+	// returns an array that stores each fact in an element
+	public String[] toArray(Scanner facts) {
+		List<String> factList = new ArrayList<String>();
+		while (facts.hasNextLine()) {
+			String line = facts.nextLine().trim();
+			factList.add(line);
+		}
+		this.facts = new String[factList.size()];
+		for (int i = 0; i < factList.size(); i++) {
+			this.facts[i] = factList.get(i);
+		}
+		return this.facts;
+	}
+
+	// Returns a random fact
+	public String getFact() {
+		Random r = new Random();
+		return this.facts[r.nextInt(this.facts.length)];
+	}
+
+	// This method takes in a String fact as a parameter
+	// and reverses it so it is backwards. 
+	public String reverseFact(String fact) {
+		return new StringBuffer(fact).reverse().toString();
+	}
+
+	// This method takes an int number and a String response
+	// to print out the facts in regular order or reverse,
+	// depending on user response. 
+	public void printFact(int number, String response) {
+		int characters = 0;
+		int words = 0;
+		for (int i = 1; i <= number; i ++) {
+			String fact = this.getFact();
+			if (response.startsWith("y") || response.contains("sure")) {
+				System.out.println("Reversed Fact #" + i + ": " 
+						+ this.reverseFact(fact));
+			} else { // user types no, so print regular facts
+				System.out.println("Fact #" + i + ": " + fact);
+			}
+			characters += this.characterCount(fact);
+			words += this.wordCount(fact);
+		}
+		System.out.println();
+		System.out.println("Total characters: " + characters);
+		System.out.println("Total words: " + words);
+	}
+
+	// This method takes a String fact as a parameter and returns 
+	// the number of characters the fact has.
+	public int characterCount(String fact) {
+		return fact.length();
+	}
+	
+	// This method takes a String fact as a parameter and returns
+	// the number of words the fact has. 
+	public int wordCount(String fact) {
+		// Splits the words by the spaces
+		String[] words = fact.trim().split("\\s+");
+		return words.length;
+	}
+}
---
+++ src/FactAppMain.java
@@ -0,0 +1,93 @@
+// Author: Yulong Tan
+// Date: 5.23.115
+// FactAppMain contains a program that generates n number of
+// random facts based on user input. It then prints the facts
+// out based on the input file. Afterwards, the program will
+// ask if the user wants to play again. 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FactAppMain {
+	// If true, no matter what the user enters, the file used will
+	// always be facts.txt. Must be set to false if they want to switch
+	// files in the file method. 
+	public static final boolean FORCE = true;;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner console = new Scanner(System.in);
+		giveIntro();
+		FactApp facts = new FactApp();
+		Scanner input = new Scanner(new File(getFile(console)));
+		// stores the facts line by line into an array
+		facts.toArray(input);
+		printFact(facts, console);
+	}
+	
+	public static void giveIntro() {
+		System.out.println("This program will try to simulate the FunFacts app I wrote");
+		System.out.println("on Android Studio and just like the one there, this can have");
+		System.out.println("multiple facts repeating itself, since there's a Random object");
+		System.out.println("being used.");
+		System.out.println();
+	}
+	
+	// This method takes in a Scanner as a parameter that asks the user
+	// for the file to use and returns that file. If force is set to true,
+	// this will always return the facts.txt file.
+	public static String getFile(Scanner console) {
+		System.out.print("What file do you want to use? ");
+		String file = console.next();
+		if (FORCE) {
+			file = "facts.txt";
+		}
+		return file;
+	}
+	
+	// This method takes in the FactApp object and a Scanner as parameters
+	// and prints out a number of random facts determined by the user. It also
+	// prompts the user if they want the fact to be reversed. If yes, it reverses it,
+	// else, it gets ignored. 
+	public static void printFact(FactApp facts, Scanner console) {
+		System.out.print("How many random facts do you want to generate? ");
+		int number = console.nextInt();
+		System.out.print("Do you want the facts completely reversed? (y/n) ");
+		String response = console.next().trim().toLowerCase();
+		facts.printFact(number, response);
+		moreFacts(facts, console);
+	}
+	
+	// This method takes in a FactApp object and a Scanner as parameters 
+	// and prompts the user to see if they want to play the game again.
+	// It will keep prompting the user until they enter something that
+	// starts with a y or an n. Case is ignored. 
+	public static void moreFacts(FactApp facts, Scanner console) {
+		System.out.println();
+		System.out.print("Do you want some more facts? (y/n) ");
+		String response = console.next().trim().toLowerCase();
+		if (response.startsWith("y") || response.contains("sure")) {
+			switchFile(facts, console);
+			printFact(facts, console);
+		} else if (response.startsWith("n")) {
+			System.out.println();
+			System.out.println("Thanks for trying out this program!");
+			System.out.println("It was nice showing you random facts!");
+			System.out.println("Have a nice day!");
+		} else { // user types neither yes or no and that's problematic
+			System.out.println("That's not a valid response!");
+			System.out.println("Please give me a response that makes sense!");
+			moreFacts(facts, console);
+		}
+	}
+	
+	// This method takes in a Scanner to see if the user wants to
+	// switch files to use for the list of facts. 
+	public static void switchFile(FactApp facts, Scanner console) {
+		System.out.print("Do you want to switch files? (y/n) ");
+		String response = console.next().trim().toLowerCase();
+		if (response.startsWith("y") || response.contains("sure")) {
+			getFile(console);
+		}
+	}
+}
